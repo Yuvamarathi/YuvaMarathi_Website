2d3dce8bbaed7f765f9334b95a9ec115298934bf
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2021-09-28 08:43:49 +0530

    Add files via upload

---
+++ Info.js
@@ -0,0 +1,74 @@
+
+const info_zero =   //done
+    [
+        { "Name": "Mrunmayi Mane", "Subname": "Events Head", "Image": "Images/Board2122/Mrunmayi Mane_Events Head (2).jpeg" },
+        { "Name": "Anurag Gumaste", "Subname": "Operations Head", "Image": "Images/Board2122/Anurag Gumaste _ Operations Head.jpg" },
+        { "Name": "Apurv Shewale", "Subname": "Management Head", "Image": "Images/Board2122/Apurv Shewale _ Management Head.jpg" },
+        { "Name": "Amogh Hatkar", "Subname": "Secretary", "Image": "Images/Board2122/Amogh Hatkar Secretary.jpg" },
+        { "Name": "Tanishq Gandhi", "Subname": "Technical Head", "Image": "Images/Board2122/Tanishq Gandhi- Technical Head.jpg" },
+        { "Name": "Ishan Jogalekar", "Subname": "Design Head", "Image": "Images/Board2122/Ishan Jogalekar Design Head.jpg" },
+        { "Name": "Vedant Karale", "Subname": "PR and Drama Head", "Image": "Images/Board2122/Vedant Karale_PR and Drama Head.jpg" },
+        { "Name": "Krantdarshi Mirajkar", "Subname": "Editorial Head", "Image": "Images/Board2122/Krantdarshi Mirajkar_Editorial head.jpg" },
+        { "Name": "Janhavi Bhosale", "Subname": "Dance Head", "Image": "Images/Board2122/Janhavi Bhosale _ Dance head.jpg" },
+        { "Name": "Anushka Pawar", "Subname": "Music Head", "Image": "Images/Board2122/Anushka Pawar _ Music Head.jpg" }
+        //{ "Name": "Piyush Waghirkar", "Subname": "Finance Head", "Image": "Images/Board2122/Piyush Waghirkar Finance Head.jpg" },
+    ];
+
+const info_one =    //done
+    [
+        { "Name": "Rucha Jagtap", "Subname": "Chairperson", "Image": "Images\\Board2021\\Rucha Jagtap chairperson.jpg" },
+        { "Name": "Atharva Wagh", "Subname": "Vice Chairperson", "Image": "Images\\Board2021\\Atharva Wagh - Vice chairperson.jpg" },
+        { "Name": "Amit Manthekar", "Subname": "Secretary", "Image": "Images\\Board2021\\Amit Manthekar - Secretary.jpg" },
+        { "Name": "Om More", "Subname": "Technical Head", "Image": "Images\\Board2021\\_Om more_Technical head_.jpg" },
+        { "Name": "Soham Shinde", "Subname": "Finance Head", "Image": "Images\\Board2021\\Soham Girish Shinde_Finance Head.jpg" },
+        { "Name": "Utkarsh Deshmukh", "Subname": "PR and Sponsorship Head", "Image": "Images\\Board2021\\Utkarsh Deshmukh PR and Sponsorship Head.JPG" },
+        { "Name": "Riya Jadhav", "Subname": "Design Head", "Image": "Images\\Board2021\\Riya Jadhav- Design H_.jpg" },
+        { "Name": "Adesh Chougule", "Subname": "Marketing Head", "Image": "Images\\Board2021\\Adesh Chougule Marketing Head.jpg" },
+        { "Name": "Yatin Kinare", "Subname": "Editorial Head", "Image": "Images\\Board2021\\_ Yatin Pundalik Kinare_ Editorial Head.png" },
+        { "Name": "Atharv kulkarni", "Subname": "Drama Head", "Image": "Images\\Board2021\\Atharv kulkarni - Drama Academy head.jpg" },
+        { "Name": "Sanchita Suryavanshi", "Subname": "Dance Head", "Image": "Images\\Board2021\\Sanchita Suryavanshi- Dance Head.jpg" }
+    ];
+
+const info_two =    //Remaining
+    [
+        { "Name": "Varad Abhyankar", "Subname": "Secretary", "Image": "Images\\Board1920\\Varad Abhyankar_Secretary.jpg" },
+        { "Name": "Shravani Shinde", "Subname": "Vice President", "Image": "Images\\Board1920\\Shravani Shinde Vice President.jpg" },
+        { "Name": "Tanuj Warpade", "Subname": "Chairperson", "Image": "Images\\Board1920\\Tanuj Warpade _ Chairperson.jpg" },
+        { "Name": "Abhishek Patil", "Subname": "Vice Chairperson Secretary", "Image": "Images\\Board1920\\Abhishek Patil_Vice Chairperson.jpg" },
+
+        { "Name": "Manjiri Dighade", "Subname": "Marketing head", "Image": "Images\\Board1920\\Manjiri Dighade_ Marketing head.JPG" },
+        { "Name": "Samved Naykalkar", "Subname": "Finance Head", "Image": "Images\\Board1920\\Samved Naykalkar - Finance Head.jpeg" },
+        { "Name": "Mohit Amdhare", "Subname": "PR, Sponsorship and Design Head", "Image": "Images\\Board1920\\Mohit Amdhare_PR, Sponsorship _ Design Head.jpg" },
+        { "Name": "Aditya Bhosale", "Subname": " Editorial Head", "Image": "Images\\Board1920\\Aditya Bhosale Editorial Head 2019-20.jpg" },
+
+        { "Name": "Tejas Mahulkar", "Subname": "Drama Head", "Image": "Images\\Board1920\\Tejas Mahulkar - Drama Head.jpg" },
+        { "Name": "Kalyani Kulkarni", "Subname": "Cultural and Dance Head", "Image": "Images\\Board1920\\Kalyani Kulkarni _ Cultural (Dance) Head.jpg" },
+    ];
+
+const info_three =  //done
+    [
+        { "Name": "Tukaram Jagtap", "Subname": "Chairperson", "Image": "Image 1.jpeg" },
+        { "Name": "Shalaka Mahurkar", "Subname": "Vice Chairperson", "Image": "Images\\Board1819\\Shalaka Shashank Mahurkar_Vice President.jpg" },
+        { "Name": "Vedant Kadam", "Subname": "Management Head", "Image": "Image 1.jpeg" },
+        { "Name": "Vaishnavi Deshpande", "Subname": "PR Head", "Image": "Images\\Board1819\\Vaishnavi Deshpande__01.jpg" },
+        { "Name": "Ashutosh Nandanwar", "Subname": "Design and Marketing Head", "Image": "Image 1.jpeg" },
+        { "Name": "Mahesh Patil", "Subname": "Finance Head", "Image": "Image 1.jpeg" },
+        { "Name": "Prachi Khadilkar", "Subname": "G. Secretary", "Image": "Image 1.jpeg" },
+        { "Name": "Vaishnavi Ingale", "Subname": "HR", "Image": "Image 1.jpeg" },
+        { "Name": "Padmanabh Deshpande", "Subname": "Technical Head", "Image": "Image 1.jpeg" },
+        { "Name": "Prajyot Jadhav", "Subname": "Editorial Head", "Image": "Image 1.jpeg" }
+    ];
+
+const info_four =   //done
+    [
+        { "Name": "Indrajeet Kenjale", "Subname": "President", "Image": "Images\\Board1718\\IndrajeetKenjale.JPG" },
+        { "Name": "Priyanka Dani", "Subname": "Vice President, Events Head", "Image": "Images\\Board1718\\PriyankaDani.png" },
+        { "Name": "Aboli Vartak", "Subname": "Secretary", "Image": "Image 1.jpeg" },
+        { "Name": "Saurabh Zaware", "Subname": "Treasurer", "Image": "Image 1.jpeg" },
+        { "Name": "Ojaswi Sumbh", "Subname": "PR Head", "Image": "Images\\Board1718\\Ojaswi Sumbh_PR Head .jpg" },
+        { "Name": "Kiran Kulkarni", "Subname": "Marketing Head", "Image": "Images\\Board1718\\KiranKulkarni_MarketingHead.jpg" },
+        { "Name": "Utkarsh Ghadge", "Subname": "Design Head", "Image": "Image 1.jpeg" },
+        { "Name": "Pranit Amrutkar", "Subname": "Design Sub Head", "Image": "Image 1.jpeg" },
+        { "Name": "Chaitanya Inamdar", "Subname": "Events Sub Head", "Image": "Image 1.jpeg" },
+        { "Name": "Dhanashri Patil", "Subname": "Technical Head", "Image": "Image 1.jpeg" }
+    ];
